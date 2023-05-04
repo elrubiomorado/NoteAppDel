f5bafca6614f038f013bd3ae5dc5631cc5179c1f
Edgar Avila Gonzalez <avilaedgar2001@gmail.com>
2023-05-04 08:21:33 -0600

    Primer commit con unos errores en metodo update

---
+++ app/src/main/java/com/del/notesappdel/Models/NoteViewModel.kt
@@ -0,0 +1,36 @@
+package com.del.notesappdel.Models
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.del.notesappdel.Database.NoteDatabase
+import com.del.notesappdel.Database.NotesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application) : AndroidViewModel(application){
+    private val repository : NotesRepository
+
+    val allnotes : LiveData<List<Note>>
+
+    init{
+        val dao = NoteDatabase.getDatabase(application).getNoteDao()
+        repository = NotesRepository(dao)
+        allnotes = repository.allNotes
+    }
+
+    fun deleteNote(note: Note) = viewModelScope.launch(Dispatchers.IO){
+        repository.delete(note)
+    }
+
+    fun insertNote(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(note)
+    }
+
+    fun updateNote(note:Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.update(note)
+    }
+
+
+}
---
+++ app/src/main/java/com/del/notesappdel/utilities/constants.kt
@@ -0,0 +1,3 @@
+package com.del.notesappdel.utilities
+
+const val DATABASE_NAME = "note_database"
